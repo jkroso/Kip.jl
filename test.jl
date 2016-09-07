17b6d2def1ad4f9f1338503d3f79896901420448
Jake Rosoman <jkroso@gmail.com>
2016-09-08 00:29:45 +1200

    update test

--- test.jl
+++ test.jl
@@ -1,8 +1,4 @@
 import Kip
 
-path = Kip.download("http://github.com/jkroso/Jest.jl/tarball/f015257")
-@assert ispath(joinpath(path, "main.jl"))
-
-@assert Kip.resolve_gh("github.com/jkroso/Jest.jl@0") == ("http://github.com/jkroso/Jest.jl/tarball/0.0.3","")
-@assert Kip.latest_gh_commit("jkroso", "Kip.jl")[1:7] == readall(`git log -n 1 --oneline`)[1:7]
-@assert Kip.resolve_gh_tag("jkroso", "Jest.jl", "0") == v"0.0.3"
+path = Kip.resolve_github("github.com/jkroso/Jest.jl@0.0.1")
+@assert path[end-39:end] == "2e6ab717b4dde474ea55d988588ba3b72a42b933"
