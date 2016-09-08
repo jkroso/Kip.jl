452263b3de90949efbb7a78ad0da64279854bf8b
Jake Rosoman <jkroso@gmail.com>
2016-09-08 15:39:40 +1200

    update

--- .juliarc.jl
+++ .juliarc.jl
@@ -1,10 +1,1 @@
 using Kip
-
-# If we are running a file and not at the REPL
-if !isinteractive() && !isempty(ARGS)
-  # set Kip.entry to the dirname of the file being run
-  Kip.eval(:(entry=$(dirname(realpath(joinpath(pwd(), ARGS[1]))))))
-end
-
-# a lot of my packages depend on this being loaded
-@require "github.com/jkroso/Jest.jl@8cfc487"
